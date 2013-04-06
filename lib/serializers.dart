dbebc2e3f2eda225d75cf78c8c87e6a06b3b79cc
Victor Savkin <vic.savkin@gmail.com>
2013-04-06 19:44:33 -0400

    init commit

---
+++ lib/serializers.dart
@@ -0,0 +1,3 @@
+library serializers;
+
+import 'dart:async';
---
+++ test/serializers_test.dart
@@ -0,0 +1,8 @@
+library serializers_test;
+
+import 'package:unittest/unittest.dart';
+import 'package:dartmocks/dartmocks.dart';
+import 'package:serializers/serializers.dart';
+
+main(){
+}
